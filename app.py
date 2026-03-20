cab875e0d7c6be34ee5accfcc977140c66dff758
lys <151134229+s27688473@users.noreply.github.com>
2026-03-20 15:02:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
+import psycopg2
+import os
 
 app = Flask(__name__)
 
+# 從 App Service 環境變數取得連線字串
+conn = psycopg2.connect(os.environ['POSTGRES_CONN'])
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+
+        if name:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO users (name) VALUES (%s)", (name,))
+            conn.commit()
+            cur.close()
+
+    # 查詢資料
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM users ORDER BY id DESC LIMIT 10")
+    rows = cur.fetchall()
+    cur.close()
+
+    return render_template("index.html", rows=rows)
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
